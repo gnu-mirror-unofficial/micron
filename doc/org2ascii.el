1a063e43c4c8c869aa2b4526f82f2067275ded77
Sergey Poznyakoff <gray@gnu.org>
2021-01-19 11:02:07 +0200

    Update documentation

--- doc/org2ascii.el
+++
@@ -1,7 +0,0 @@
-(let ((bufname (car command-line-args-left))
-      (target (car (cdr command-line-args-left))))
-  (set-buffer (find-file bufname))
-  (let ((name (org-ascii-export-to-ascii)))
-    (if target
-	(rename-file name target t))
-    (kill-buffer (current-buffer))))
