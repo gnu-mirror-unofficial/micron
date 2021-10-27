812e26197db4eed78f9704a3e67c134187ecffb7
Sergey Poznyakoff <gray@gnu.org>
2021-10-27 09:44:24 +0300

    Fixes in testsuite
    * README: Document the version of libfaketime needed.
    * tests/Makefile.am: Link ftt with pthreads.
    * tests/ftt.c: Rewrite to test for working pthread_cond_timedwait.
    * tests/outfile.at: Compensate for start time ambiguity

--- tests/ftt.c
+++ tests/ftt.c
@@ -9,12 +9,18 @@
     Argument must be a full pathname of the libfaketime.so library, or
     a colon-delimited list of library pathnames (one of them being
     libfaketime), suitable for use in LD_PRELOAD variable.  The program
-    spawns a copy of itself with LD_PRELOAD set to LIBNAME, FAKETIME
-    set to a predefined value, and analyzes the output and time it took
-    to produce it.
+    spawns a copy of itself with LD_PRELOAD set to LIBNAME, and FAKETIME
+    set to one minute from now with a speedup of 60 times (one program's
+    minute = one realtime second).  The spawned copy starts a thread that
+    calls pthread_cond_timedwait with the time limit set to one minute from
+    now, and joins that thread.  The master waits for three seconds for
+    the child to terminate (in fact, it should take little longer than
+    one second) and returns 0 if the child exits in time, meaning that
+    libfaketime.so is usable, and 1 otherwise.
 
-    If libfaketime.so is usable, exits with code 0.  Otherwise, exits
-    with code 1.
+  NOTE
+    Support for pthread_cond_timedwait appeared in version 0.9.8 of
+    libfaketime (commit fb91c4fcde).  It is crucial for micron testsuite.
 
   LICENSE
     Copyright (C) 2020-2021 Sergey Poznyakoff
@@ -38,30 +44,35 @@
 #include <assert.h>
 #include <errno.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sys/time.h>
 #include <sys/wait.h>
 
-static char expout[] = "1609459200\n\
-1609459260\n\
-1609459320\n\
-1609459380\n\
-1609459440\n";
+static pthread_mutex_t thr_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t thr_cond = PTHREAD_COND_INITIALIZER;
+
+void *
+thr_wait(void *ptr)
+{
+    pthread_mutex_lock(&thr_mutex);
+    pthread_cond_timedwait(&thr_cond, &thr_mutex, ptr);
+    pthread_mutex_unlock(&thr_mutex);
+    return NULL;
+}
 
 int
 main(int argc, char **argv)
 {
+    struct timespec now, ts;
+
+    clock_gettime(CLOCK_REALTIME, &now);
+    now.tv_sec += 60;
+	
     if (argc == 2) {
 	pid_t pid;
 	int status;
-	struct timespec ts = { 3, 0 };
 	sigset_t sigs;
-	int p[2];
-	
-	if (pipe(p)) {
-	    perror("pipe");
-	    return 1;
-	}
 	
 	pid = fork();
 	if (pid == -1) {
@@ -70,63 +81,38 @@ main(int argc, char **argv)
 	}
 
 	if (pid == 0) {
-	    if (dup2(p[1], 1) == -1) {
-		perror("dup2");
-	    } else {
-		setenv("TZ", "GMT", 1);
-		setenv("LD_PRELOAD", argv[1], 1);
-		setenv("FAKETIME", "@2021-01-01 00:00:00 x120", 1);
-		execlp(argv[0], argv[0], NULL);
-		perror("execlp");
-	    }
+	    struct tm *tm;
+	    char tbuf[sizeof("@1970-01-01 00:00:00 x60")];
+	    
+	    tm = localtime(&now.tv_sec);
+	    strftime(tbuf, sizeof(tbuf), "@%Y-%m-%d %H:%M:%S x60", tm);
+	    setenv("LD_PRELOAD", argv[1], 1);
+	    setenv("FAKETIME", tbuf, 1);
+	    execlp(argv[0], argv[0], NULL);
+	    perror("execlp");
 	    _exit(127);
 	}
 
-	close(p[1]);
-	
 	sigemptyset(&sigs);
 	sigaddset(&sigs, SIGCHLD);
 	sigprocmask(SIG_BLOCK, &sigs, NULL);
 
+	ts.tv_sec = 3;
+	ts.tv_nsec = 0;
 	status = sigtimedwait(&sigs, NULL, &ts);
 	if (status == -1) {
 	    kill(pid, SIGKILL);
 	    return 1;
 	} else {
-	    char buf[sizeof(expout)-1];
-	    ssize_t n;
-	    
 	    wait(&status);
 	    if (!(WIFEXITED(status) && WEXITSTATUS(status) == 0))
 		return 1;
-
-	    n = read(p[0], buf, sizeof(buf));
-	    if (n != sizeof(buf))
-		return 1;
-
-	    if (memcmp(buf, expout, n))
-		return 1;
-
-	    n = read(p[0], buf, sizeof(buf));
-	    if (n != 0)
-		return 1;
 	}
     } else {
-	struct timespec to = { 60, 0 };
-	int i, count = 5;
-    
-	for (i = 0;;) {
-	    struct timespec ts;
-	    clock_gettime(CLOCK_REALTIME, &ts);
-	    printf("%ld\n", ts.tv_sec);
-	    if (++i == count)
-		break;
-	    ts = to;
-	    if (nanosleep(&ts, NULL)) {
-		perror("nanosleep");
-		return 1;
-	    }
-	}
+	pthread_t tid;
+	void *res;	
+	pthread_create(&tid, NULL, thr_wait, &now);
+	pthread_join(tid, &res);
     }
     return 0;
 }
